3eefb870c9451d66632b590ab6782048a489a16c
hugo <hugosousa76@gmail.com>
2022-04-06 20:45:47 -0300

    updated fetch request with post

--- backend/server.js
+++ backend/server.js
@@ -14,12 +14,11 @@ function initClient()  {
     })
 }
 
-const query = 'SELECT * FROM points'
 
-async function connection() {
+async function connection(query, params = null) {
     const client = initClient()
     client.connect()
-    const res = await client.query(query)
+    const res = await client.query(query, params)
     const resultado = res.rows[0]
     console.log(resultado)
     await client.end()
@@ -31,9 +30,34 @@ const corsOptions = {
 }
 
 server.use(cors(corsOptions))
+server.use(express.json())
 
 server.get('/getname', async (req, res) => {
-    const json = await connection()
+    const query = 'SELECT * FROM points'
+    const json = await connection(query)
+    res.json(json)
+})
+
+server.post('/create-point', async (req, res) => {
+    const {number, date, time, latitude, longitude} = req.body
+    const query = "INSERT INTO points (number, date, time, latitude, longitude) VALUES ($1, $2, $3, $4, $5)"
+    const json = await connection(query, [number, date, time, latitude, longitude])
+    res.json(json)
+})
+
+server.delete('/delete-point/:id', async(req, res) => {
+    const query = 'DELETE FROM points WHERE number = $1'
+    const number = parseInt(req.params.id)
+    const json = await connection(query,[number])
+    res.json(json)
+})
+
+server.put('/update-point/:id', async(req, res) => {
+    const query = 'UPDATE points SET latitude = $1, longitude = $2 WHERE number = $3'
+    const number = parseInt(req.params.id)
+    const lat = 333
+    const long = 666
+    const json = await connection(query,[lat, long, number])
     res.json(json)
 })
 
--- frontend/src/script.js
+++ frontend/src/script.js
@@ -1,11 +1,53 @@
 const apiURL = 'http://localhost:3000/'
 const button = document.querySelector('#button')
+const deleteButton = document.querySelector('#delete-button')
+const createButton = document.querySelector('#create-button')
+const updateButton = document.querySelector('#update-button')
 
 button.addEventListener('click', pegarUmNome)
+deleteButton.addEventListener('click', deleteOne)
+createButton.addEventListener('click', createOne)
+updateButton.addEventListener('click', updateOne)
 
 function pegarUmNome() {
     const rota = 'getname'
     fetch(apiURL + rota)
-    .then(response => response.json())
-    .then(data => console.log(data))
+        .then(response => response.json())
+        .then(data => console.log(data))
+}
+
+function deleteOne() {
+    const rota = 'delete-point/1'
+    fetch(apiURL + rota, { method: 'DELETE' })
+        .then(response => response.json())
+        .then(data => console.log(data))
+}
+
+function createOne() {
+    const rota = 'create-point'
+    const config = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify({
+            number: '1',
+            date: '01-01-2020',
+            // date: new Date().getDate(),
+            time: '12:00',
+            latitude: 100000,
+            longitude: 100000
+        })
+    }
+    fetch(apiURL + rota, config)
+        .then(response => response.json())
+        .then(data => console.log(data))
+}
+
+function updateOne() {
+    const rota = 'update-point/1'
+    fetch(apiURL + rota, { method: 'PUT' })
+        .then(response => response.json())
+        .then(data => console.log(data))
 }
