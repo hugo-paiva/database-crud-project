dfbaec67c6c8323f1410c0a883d272eb2b778d9b
hugo <hugosousa76@gmail.com>
2022-04-06 22:25:45 -0300

    minor fixes

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,12 @@ const express = require('express')
 const cors = require('cors')
 const server = express()
 const PORT = 3000
-const {Client} = require('pg')
+const { Client } = require('pg')
 
-function initClient()  {
+function initClient() {
     return new Client({
-        user:'postgres',
-        password: 'cueiozim',
+        user: 'postgres',
+        password: 'senha',
         host: 'localhost',
         port: 5432,
         database: 'Mapeamento_geologico'
@@ -18,8 +18,18 @@ function initClient()  {
 async function connection(query, params = null) {
     const client = initClient()
     client.connect()
-    const res = await client.query(query, params)
-    const resultado = res.rows[0]
+    let res
+    let resultado = {
+        content: {},
+        message: 'default'
+    }
+    try {
+        res = await client.query(query, params)
+        resultado.content = res.rows
+        resultado.message = 'Sua query foi executada com sucesso!'  
+    } catch (error) {
+        resultado.message = 'Sua query deu erro!'
+    }
     console.log(resultado)
     await client.end()
     return resultado
@@ -39,25 +49,25 @@ server.get('/getname', async (req, res) => {
 })
 
 server.post('/create-point', async (req, res) => {
-    const {number, date, time, latitude, longitude} = req.body
+    const { number, date, time, latitude, longitude } = req.body
     const query = "INSERT INTO points (number, date, time, latitude, longitude) VALUES ($1, $2, $3, $4, $5)"
     const json = await connection(query, [number, date, time, latitude, longitude])
     res.json(json)
 })
 
-server.delete('/delete-point/:id', async(req, res) => {
+server.delete('/delete-point/:id', async (req, res) => {
     const query = 'DELETE FROM points WHERE number = $1'
     const number = parseInt(req.params.id)
-    const json = await connection(query,[number])
+    const json = await connection(query, [number])
     res.json(json)
 })
 
-server.put('/update-point/:id', async(req, res) => {
+server.put('/update-point/:id', async (req, res) => {
     const query = 'UPDATE points SET latitude = $1, longitude = $2 WHERE number = $3'
     const number = parseInt(req.params.id)
     const lat = 333
     const long = 666
-    const json = await connection(query,[lat, long, number])
+    const json = await connection(query, [lat, long, number])
     res.json(json)
 })
 
--- frontend/src/script.js
+++ frontend/src/script.js
@@ -3,6 +3,13 @@ const button = document.querySelector('#button')
 const deleteButton = document.querySelector('#delete-button')
 const createButton = document.querySelector('#create-button')
 const updateButton = document.querySelector('#update-button')
+// number, date, time, latitude, longitude
+const number = document.querySelector('#number')
+const date = document.querySelector('#date')
+const time = document.querySelector('#time')
+const latitude = document.querySelector('#latitude')
+const longitude = document.querySelector('#longitude')
+
 
 button.addEventListener('click', pegarUmNome)
 deleteButton.addEventListener('click', deleteOne)
@@ -38,7 +45,15 @@ function createOne() {
             time: '12:00',
             latitude: 100000,
             longitude: 100000
-        })
+        }),
+        // body: JSON.stringify({
+        //     number: number.value,
+        //     date: date.value,
+        //     // date: new Date().getDate(),
+        //     time: time.value,
+        //     latitude: latitude.value,
+        //     longitude: longitude.value
+        // })
     }
     fetch(apiURL + rota, config)
         .then(response => response.json())
